404fe9f8d3992afdcea909162207e752f9a1d65f
georges2424 <65792413+georges2424@users.noreply.github.com>
2023-08-02 01:10:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,7 @@ MYSQL_USER = os.getenv('MYSQL_USER')
 MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
 MYSQL_DB = os.getenv('MYSQL_DB')
 
-# Connect to the database
-connection = pymysql.connect(host=MYSQL_HOST,
-                             user=MYSQL_USER,
-                             password=MYSQL_PASSWORD,
-                             db=MYSQL_DB,
-                             cursorclass=pymysql.cursors.DictCursor)
+
 
 app = Flask(__name__)
 
